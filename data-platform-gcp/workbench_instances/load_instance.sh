c2e8fec55370f3b94a1d6c63b2937b41d64fc027
iaSalazar <ivanarturo9620@gmail.com>
2025-03-12 21:53:59 -0500

    dockerfile custom dataproc template image

--- data-platform-gcp/workbench_instances/load_instance.sh
+++ data-platform-gcp/workbench_instances/load_instance.sh
@@ -12,6 +12,16 @@ gcloud workbench instances create instance-20250220-233039 \
     --location=us-central1-a \
     --machine-type=n2-standard-4 \
 
+    
+gcloud compute instances detach-disk instance-20250220-233039 \
+    --disk=instance-20250220-233039-boot \
+    --zone=us-central1-a
+
+gcloud compute instances attach-disk instance-20250220-233039 \
+    --disk=instance-20250220-233039-boot \
+    --zone=us-central1-a \
+    --boot
+
 
 gcloud workbench instances restore instance-20250220-233039 \
     --location=us-central1-a \
